a6bde673feecd4f06faa0e9f60418f4c4de5837d
Clems <clem29auv@gmail.com>
2025-07-24 16:53:50 +0200

    allow craft to be compare on specific items

---
+++ src/main/kotlin/fr/bl4/blib/item/ItemSpecificMeta.kt
@@ -0,0 +1,9 @@
+package fr.bl4.blib.item
+
+enum class ItemSpecificMeta {
+	TYPE,
+	NAME,
+	MODEL,
+	FLAGS,
+	ENCHANTMENTS
+}
--- src/main/kotlin/fr/bl4/blib/item/ItemStackUtils.kt
+++ src/main/kotlin/fr/bl4/blib/item/ItemStackUtils.kt
@@ -2,6 +2,7 @@ package fr.bl4.blib.item
 
 import net.kyori.adventure.text.Component
 import net.kyori.adventure.text.serializer.gson.GsonComponentSerializer
+import net.kyori.adventure.text.serializer.plain.PlainTextComponentSerializer
 import org.bukkit.Material
 import org.bukkit.inventory.ItemStack
 
@@ -36,3 +37,37 @@ fun Component.replaces(placeholders: Map<String, String>): Component {
  * Returns true if the ItemStack is null or [ItemStack.getType] is [Material.AIR]
  */
 fun ItemStack?.isAirOrNull(): Boolean = this == null || this.type == Material.AIR
+
+fun ItemStack.isSameSpecificMeta(itemstack: ItemStack, specificMeta: ItemSpecificMeta): Boolean {
+	val thisMeta = this.itemMeta ?: return false
+	val itemstackMeta = itemstack.itemMeta ?: return false
+
+	return when (specificMeta) {
+		ItemSpecificMeta.TYPE -> this.type === itemstack.type
+		ItemSpecificMeta.NAME -> {
+			val thisName = PlainTextComponentSerializer.plainText().serialize(this.displayName())
+			val itemstackName = PlainTextComponentSerializer.plainText().serialize(itemstack.displayName())
+
+			thisName == itemstackName
+		}
+		ItemSpecificMeta.MODEL -> thisMeta.hasCustomModelData() == itemstackMeta.hasCustomModelData()
+				&& thisMeta.customModelData == itemstackMeta.customModelData
+		ItemSpecificMeta.FLAGS -> thisMeta.itemFlags == itemstackMeta.itemFlags
+		ItemSpecificMeta.ENCHANTMENTS -> this.enchantments == itemstack.enchantments
+	}
+}
+
+fun ItemStack?.isSame(itemStack: ItemStack?, specificMetas: List<ItemSpecificMeta>?): Boolean {
+	if (this == null && itemStack == null) return true
+	if (this == null || itemStack == null) return false
+
+	if (specificMetas == null ||specificMetas.isEmpty())
+		return this.isSimilar(itemStack)
+
+	for (meta in specificMetas) {
+		if (!this.isSameSpecificMeta(itemStack, meta))
+			return false
+	}
+
+	return true
+}
