d4c532244d097a61f46616f84896085cde446f41
Clems <clem29auv@gmail.com>
2025-07-30 23:13:43 +0200

    Change all versions logic

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,58 +1,52 @@
 plugins {
-	kotlin("jvm") version "2.1.20"
-	id("com.github.johnrengelman.shadow") version "8.1.1"
+	alias(libs.plugins.kotlin)
+	alias(libs.plugins.shadow)
 }
 
 group = "fr.bl4"
-version = "1.0"
+version = "1.0.0"
 
 repositories {
 	mavenCentral()
-	maven("https://repo.papermc.io/repository/maven-public/") {
-		name = "papermc-repo"
-	}
-	maven("https://oss.sonatype.org/content/groups/public/") {
-		name = "sonatype"
-	}
-	maven("https://repo.codemc.io/repository/maven-public/") {
-		name = "CodeMC"
-	}
+	maven("https://repo.papermc.io/repository/maven-public/") { name = "papermc-repo" }
+	maven("https://oss.sonatype.org/content/groups/public/") { name = "sonatype" }
+	maven("https://repo.codemc.io/repository/maven-public/") { name = "CodeMC" }
 	maven("https://maven.devs.beer/")
 }
 
 dependencies {
-	compileOnly("io.papermc.paper:paper-api:1.21.4-R0.1-SNAPSHOT")
-	implementation("org.jetbrains.kotlin:kotlin-stdlib-jdk8")
+	compileOnly(libs.paperApi)
+	implementation(libs.kotlinStdlib)
 
-	compileOnly("de.tr7zw:item-nbt-api-plugin:2.15.0")
-	compileOnly("dev.lone:api-itemsadder:4.0.10")
-	compileOnly("net.luckperms:api:5.5")
+	compileOnly(libs.itemNbtApi)
+	compileOnly(libs.itemsAdder)
+	compileOnly(libs.luckpermsApi)
 }
 
-val targetJavaVersion = 21
 kotlin {
-	jvmToolchain(targetJavaVersion)
+	jvmToolchain(libs.versions.java.get().toInt())
 }
 
 tasks.build {
-	dependsOn("shadowJar")
+	dependsOn(tasks.shadowJar)
 }
 
 tasks.processResources {
 	val props = mapOf("version" to version)
 	inputs.properties(props)
 	filteringCharset = "UTF-8"
-	filesMatching("plugin.yml") {
+	duplicatesStrategy = DuplicatesStrategy.INCLUDE
+
+	from(sourceSets.main.get().resources) {
+		include("plugin.yml")
 		expand(props)
+	}
+
+	from(sourceSets.main.get().resources) {
+		exclude("plugin.yml")
 	}
 }
 
 tasks.shadowJar {
-	archiveFileName = "${project.name}-${project.version}.jar"
-}
-
-tasks.register<Copy>("copyToServer") {
-	dependsOn("build")
-	from(layout.buildDirectory.file("libs/${project.name}-$version.jar"))
-	into(rootProject.layout.projectDirectory.dir("server/plugins"))
+	archiveFileName.set("${project.name}-${project.version}.jar")
 }
